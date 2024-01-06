3c3708c6c54ccc30aee645c948161034afa93a11
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-06 23:38:30 +0900

    test bin

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -3,17 +3,18 @@
 import {program} from 'commander'
 import * as path from 'path'
 import * as fs from 'fs-extra'
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url'
 
 program.name('create-ham-vue3-gas').action(
   () => {
-      console.log('Initializing ham-vue3-gas')
-      const rootPath = path.resolve(process.cwd())
-      fs.copySync(path.join(__dirname, 'template/.clasp.json'), path.join(rootPath, '.clasp.json'))
-      fs.copySync(path.join(__dirname, 'template/package.json'), path.join(rootPath, 'package.json'))
-      fs.copySync(path.join(__dirname, 'template/shared'), path.join(rootPath, 'shared'))
-      fs.copySync(path.join(__dirname, 'template/frontend'), path.join(rootPath, 'frontend'))
-      fs.copySync(path.join(__dirname, 'template/backend'), path.join(rootPath, 'backend'))
+    console.log('Create ham-vue3-gas')
+    const rootPath = path.resolve(process.cwd())
+    fs.copySync(path.join(__dirname, 'template/.clasp.json'), path.join(rootPath, '.clasp.json'))
+    fs.copySync(path.join(__dirname, 'template/package.json'), path.join(rootPath, 'package.json'))
+    fs.copySync(path.join(__dirname, 'template/shared'), path.join(rootPath, 'shared'))
+    fs.copySync(path.join(__dirname, 'template/frontend'), path.join(rootPath, 'frontend'))
+    fs.copySync(path.join(__dirname, 'template/backend'), path.join(rootPath, 'backend'))
+    console.log('Successfully created')
   }
 )
 program.parse(process.argv)

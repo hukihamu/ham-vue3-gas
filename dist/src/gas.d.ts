6107b09ec02a2871cc89b912ff0f01142ab1cff1
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-06 15:15:27 +0900

    Remove unused notion.ts, spreadsheet.ts, and ssCache.ts files
    These files were previously used for handling spreadsheets and notion clients in Google Apps Script. However, they are now obsolete and have been removed to clean up the codebase. The changes are reflected in the TypeScript source and the corresponding compiled JavaScript files.

--- dist/src/gas.d.ts
+++ dist/src/gas.d.ts
@@ -3,9 +3,7 @@
 /// <reference types="google-apps-script" />
 /// <reference types="google-apps-script" />
 import { BaseScriptType } from './share';
-import * as notion from './gas/notion';
-import * as spreadsheet from './gas/spreadsheet';
-export { createGasApp, AsyncScriptType, useProperties, notion, spreadsheet };
+export { createGasApp, AsyncScriptType, useProperties };
 /**
  * URL Fetch calls	20,000 / day
  */
@@ -86,4 +84,182 @@ declare function useProperties<K extends string>(): {
         setProperty: (key: K, value: string) => GoogleAppsScript.Properties.Properties;
         deleteProperty: (key: K) => void;
     };
+};
+type DatabaseQueryParams = {
+    filter?: any;
+    sorts?: any[];
+    page_size?: number;
+};
+type PageCreateParams = {
+    parent: {
+        page_id: string;
+    } | {
+        database_id: string;
+    };
+    properties: any;
+    children?: any[] | string;
+    icon?: any;
+    cover?: any;
+};
+type PageUpdatePropertiesParams = {
+    properties?: any;
+    archived?: boolean;
+    icon?: any;
+    cover?: any;
+};
+export declare class NotionClient {
+    private readonly _apiBaseUrl;
+    private readonly _authToken;
+    private readonly _urlFetchApp;
+    constructor(authToken: string);
+    static createToken(): string;
+    private createHeaders;
+    private fetch;
+    get blocks(): {
+        append(): void;
+        get(): void;
+        list(): void;
+        update(): void;
+        delete(): void;
+    };
+    get pages(): {
+        create: (body: PageCreateParams) => Promise<any>;
+        get: (pageId: string) => Promise<any>;
+        getProperty: (pageId: string, propertyId: string) => Promise<any>;
+        updateProperty: (pageId: string, body: PageUpdatePropertiesParams) => Promise<any>;
+        archive(): void;
+    };
+    get databases(): {
+        create(): void;
+        query: (databaseId: string, body?: DatabaseQueryParams) => Promise<any>;
+        get(): void;
+        update(): void;
+        updateProperty(): void;
+    };
+    get users(): {
+        get(): void;
+        list(): void;
+        getBot(): void;
+    };
+    get comments(): {
+        create(): void;
+        get(): void;
+    };
+    get search(): {
+        searchByTitle(): void;
+    };
+}
+/**
+ * SSRepositoryのinitData、columnListの宣言に使用
+ */
+export type InitEntity<E extends SSEntity> = Omit<E, 'row'>;
+/**
+ * スプレッドシートに格納するデータオブジェクトを定義
+ */
+export type SSEntity = {
+    row: number;
+};
+/**
+ * スプレッドシートをテーブルとしてCRUD操作を行う<br>
+ * 本abstract classをextendsして作成する<br>
+ * extendsしたクラスをgasInit().useSpreadsheetDBに入力すると利用可能となる<br>
+ * extendsしたクラスをインスタンス化して利用する
+ */
+export declare abstract class SSRepository<E extends SSEntity> {
+    private _sheet;
+    private importSheet;
+    private get sheet();
+    private static readonly TABLE_VERSION_LABEL;
+    private static readonly DELETE_LABEL;
+    private static readonly ROW_FUNCTION;
+    /**
+     * テーブルバージョン<br>
+     * 変更を行うと、旧テーブルをバックアップし、新しくテーブル生成を行う<br>
+     * columnList、initData、Entity変更時にバージョンを上げる
+     * @protected
+     */
+    protected abstract readonly tableVersion: number;
+    /**
+     * スプレッドシートに保存する際のカラム順を決める
+     * @protected
+     */
+    protected abstract readonly columnOrder: (keyof InitEntity<E>)[];
+    /**
+     * テーブル作成(アップデート)時、初期にInsertされるデータ
+     * @protected
+     */
+    protected readonly initData: InitEntity<E>[];
+    /**
+     * データ格納に利用するスプレッドシートID(d/{スプレッドシートID}/edit)
+     * @protected
+     */
+    protected abstract readonly spreadsheetId: string;
+    /**
+     * テーブル名(シート名)
+     * @protected
+     */
+    protected abstract readonly tableName: string;
+    /**
+     * SpreadsheetApp(OAuth スコープ回避のため)
+     * @protected
+     */
+    protected readonly spreadSheetApp: GoogleAppsScript.Spreadsheet.SpreadsheetApp;
+    /**
+     * トランザクションタイプ(LockService参照) default: user
+     */
+    lockType: LockType;
+    /**
+     * トランザクションロック開放を待つ時間(ミリ秒)
+     */
+    lockWaitMSec: number;
+    private checkRequiredUpdate;
+    private createTable;
+    private toStringList;
+    private toEntity;
+    private getRowRange;
+    private onLock;
+    /**
+     * gas console上で動作させるinitTables()で利用される
+     */
+    initTable(): void;
+    /**
+     * 挿入処理
+     * @param entity 挿入するデータ。rowの有無は任意(利用せず、新規rowが付与される)
+     * @return 挿入したデータのrow
+     */
+    insert(entity: E | InitEntity<E>): number;
+    /**
+     * 全件取得(フィルターなどはJSで実施)
+     */
+    getAll(): E[];
+    /**
+     * １件取得
+     * @param row 取得するrow(rowは自動で付与され、不定一意)
+     */
+    getByRow(row: number): E;
+    /**
+     * 更新処理(上書きなため、部分変更不可)
+     * @param entity 変更するデータ(row 必須)
+     */
+    update(entity: E): void;
+    /**
+     * 削除処理
+     * @param row 削除するrow(rowは自動で付与され、不定一意)
+     */
+    delete(row: number): void;
+}
+type LockType = 'user' | 'script' | 'none';
+/**
+ * SpreadsheetをDBとして利用する<br>
+ * 作成したRepositoryを登録する
+ */
+export declare function useSpreadsheetDB(initGlobal: (global: {
+    initTables: () => void;
+}, initTables: () => void) => void, ...repositoryList: {
+    new (): SSRepository<any>;
+}[]): void;
+export declare function spreadsheetCache(spreadsheetId: string, expirationInSeconds?: number): {
+    get: (rowNumber: number) => any;
+    set: (rowNumber: number, data: any) => void;
+    clear: (rowNumber: number) => void;
 };
--- dist/src/gas.js
+++ dist/src/gas.js
@@ -1,6 +1,4 @@
-import * as notion from './gas/notion';
-import * as spreadsheet from './gas/spreadsheet';
-export { createGasApp, useProperties, notion, spreadsheet };
+export { createGasApp, useProperties };
 let useGasAPI = {};
 function createGasApp(options = {}) {
     global.doGet = () => {
@@ -92,6 +90,417 @@ function useProperties() {
             deleteProperty: (key) => {
                 PropertiesService.getUserProperties().deleteProperty(key);
             },
+        }
+    };
+}
+export class NotionClient {
+    _apiBaseUrl = 'https://api.notion.com/v1';
+    _authToken;
+    _urlFetchApp;
+    constructor(authToken) {
+        this._authToken = authToken;
+        if (useGasAPI.urlFetchApp) {
+            this._urlFetchApp = useGasAPI.urlFetchApp;
+        }
+        else {
+            throw 'not found UrlFetchApp. run "createGasApp({useGasAPI})"';
+        }
+    }
+    static createToken() {
+        // TODO GAS Oauth2を利用する
+        //  https://qiita.com/Qnoir/items/98741f6b4266e6960b9d
+        //  https://developers.notion.com/reference/create-a-token
+        return '';
+    }
+    createHeaders() {
+        return {
+            Authorization: `Bearer ${this._authToken}`,
+            'Notion-Version': '2022-06-28',
+            'Content-Type': 'application/json'
+        };
+    }
+    async fetch(path, options) {
+        const url = this._apiBaseUrl + path;
+        let resp = this._urlFetchApp.fetch(url, options);
+        if (resp.getResponseCode() === 429) {
+            await new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve();
+                }, 2000);
+            });
+            resp = this._urlFetchApp.fetch(url, options);
+        }
+        if (resp.getResponseCode() === 200) {
+            return JSON.parse(resp.getContentText());
+        }
+        throw resp.getContentText();
+    }
+    get blocks() {
+        return {
+            append() { },
+            get() { },
+            list() { },
+            update() { },
+            delete() { },
+        };
+    }
+    get pages() {
+        return {
+            create: async (body) => {
+                return this.fetch('/pages', {
+                    headers: this.createHeaders(),
+                    method: 'post',
+                    payload: JSON.stringify(body),
+                    muteHttpExceptions: true,
+                });
+            },
+            get: async (pageId) => {
+                return this.fetch(`/pages/${pageId}`, {
+                    headers: this.createHeaders(),
+                    method: 'get',
+                    muteHttpExceptions: true,
+                });
+            },
+            getProperty: async (pageId, propertyId) => {
+                return this.fetch(`/pages/${pageId}/properties/${propertyId}`, {
+                    headers: this.createHeaders(),
+                    method: 'get',
+                    muteHttpExceptions: true,
+                });
+            },
+            updateProperty: async (pageId, body) => {
+                return this.fetch(`/pages/${pageId}`, {
+                    headers: this.createHeaders(),
+                    method: 'patch',
+                    payload: JSON.stringify(body),
+                    muteHttpExceptions: true,
+                });
+            },
+            archive() { },
+        };
+    }
+    get databases() {
+        return {
+            create() {
+            },
+            query: async (databaseId, body = {}) => {
+                let cursor = undefined;
+                let result = [];
+                while (true) {
+                    const payload = Object.assign(body, { start_cursor: cursor });
+                    const resp = await this.fetch(`/databases/${databaseId}/query`, {
+                        headers: this.createHeaders(),
+                        method: 'post',
+                        payload: JSON.stringify(payload),
+                        muteHttpExceptions: true,
+                    });
+                    result = result.concat(resp.results);
+                    if (resp.has_more) {
+                        cursor = resp.next_cursor;
+                    }
+                    else {
+                        return result;
+                    }
+                }
+            },
+            get() {
+            },
+            update() {
+            },
+            updateProperty() {
+            },
+        };
+    }
+    get users() {
+        return {
+            get() { },
+            list() { },
+            getBot() { },
+        };
+    }
+    get comments() {
+        return {
+            create() { },
+            get() { },
+        };
+    }
+    get search() {
+        return {
+            searchByTitle() { },
+        };
+    }
+}
+/**
+ * スプレッドシートをテーブルとしてCRUD操作を行う<br>
+ * 本abstract classをextendsして作成する<br>
+ * extendsしたクラスをgasInit().useSpreadsheetDBに入力すると利用可能となる<br>
+ * extendsしたクラスをインスタンス化して利用する
+ */
+export class SSRepository {
+    _sheet;
+    importSheet() {
+        const spreadsheet = this.spreadSheetApp.openById(this.spreadsheetId);
+        return spreadsheet.getSheetByName(this.tableName) ?? undefined;
+    }
+    get sheet() {
+        if (!this._sheet) {
+            this._sheet = this.importSheet();
+            if (this._sheet) {
+                if (this.checkRequiredUpdate(this._sheet)) {
+                    throw `not updated Sheet "${this.tableName}" gas editor run "initTables"`;
+                }
+                else {
+                    return this._sheet;
+                }
+            }
+            throw `not found Sheet "${this.tableName}" gas editor run "initTables"`;
+        }
+        else {
+            return this._sheet;
+        }
+    }
+    static TABLE_VERSION_LABEL = 'ver:';
+    static DELETE_LABEL = 'DELETE';
+    static ROW_FUNCTION = '=row()';
+    /**
+     * テーブル作成(アップデート)時、初期にInsertされるデータ
+     * @protected
+     */
+    initData = [];
+    /**
+     * SpreadsheetApp(OAuth スコープ回避のため)
+     * @protected
+     */
+    spreadSheetApp = useGasAPI.spreadsheetApp ?? (() => {
+        throw 'not found SpreadsheetApp. run "createGasApp({useGasAPI})"';
+    })();
+    /**
+     * トランザクションタイプ(LockService参照) default: user
+     */
+    lockType = 'user';
+    /**
+     * トランザクションロック開放を待つ時間(ミリ秒)
+     */
+    lockWaitMSec = 10000;
+    checkRequiredUpdate(sheet) {
+        return sheet.getRange(1, 1, 1, 1).getValue() !== SSRepository.TABLE_VERSION_LABEL + this.tableVersion;
+    }
+    createTable(sheet) {
+        // DataRangeが1行より多い場合、データはあると判断
+        if (sheet.getDataRange().getValues().length > 1) {
+            const oldVersion = sheet.getRange(1, 1, 1, 1).getValue();
+            const oldSheet = sheet.copyTo(this.spreadSheetApp.openById(this.spreadsheetId));
+            const oldName = sheet.getName() + ' version:' + oldVersion;
+            oldSheet.setName(oldName);
+            sheet.clear();
+        }
+        // バージョン情報をセット
+        sheet.getRange(1, 1, 1, 1).setValue(SSRepository.TABLE_VERSION_LABEL + this.tableVersion);
+        //ヘッダーをセット
+        sheet.getRange(1, 2, 1, this.columnOrder.length).setValues([this.columnOrder]);
+        //初期データをインサート
+        for (const e of this.initData) {
+            this.insert(e);
+        }
+    }
+    toStringList(entity) {
+        const result = [];
+        result.push(SSRepository.ROW_FUNCTION);
+        for (const key of this.columnOrder) {
+            const value = entity[key] ?? '';
+            result.push(JSON.stringify(value));
+        }
+        return result;
+    }
+    toEntity(stringList) {
+        const entity = {
+            row: stringList[0],
+        };
+        for (let i = 1; i < stringList.length; i++) {
+            const key = this.columnOrder[i - 1];
+            entity[key] = JSON.parse(stringList[i] ?? '');
+        }
+        return entity;
+    }
+    getRowRange(rowNumber) {
+        return this.sheet.getRange(rowNumber, 1, 1, this.columnOrder.length + 1);
+    }
+    onLock(runningInLock) {
+        if (this.lockType === 'none')
+            return runningInLock();
+        const lock = this.lockType === 'user' ? LockService.getUserLock() : LockService.getScriptLock();
+        try {
+            lock.waitLock(this.lockWaitMSec);
+            const result = runningInLock();
+            this.spreadSheetApp.flush();
+            return result;
+        }
+        finally {
+            lock.releaseLock();
+        }
+    }
+    /**
+     * gas console上で動作させるinitTables()で利用される
+     */
+    initTable() {
+        // シートがない場合生成する必要がある
+        const spreadsheet = this.spreadSheetApp.openById(this.spreadsheetId);
+        const sheet = spreadsheet.getSheetByName(this.tableName);
+        this._sheet = sheet ? sheet : spreadsheet.insertSheet().setName(this.tableName);
+        if (this.checkRequiredUpdate(this._sheet)) {
+            this.createTable(this._sheet);
+        }
+    }
+    /**
+     * 挿入処理
+     * @param entity 挿入するデータ。rowの有無は任意(利用せず、新規rowが付与される)
+     * @return 挿入したデータのrow
+     */
+    insert(entity) {
+        return this.onLock(() => {
+            let insertRowNumber = -1;
+            const values = this.sheet.getDataRange().getValues();
+            for (let i = 1; i < values.length; i++) {
+                if ((values[i] ?? [])[0] === SSRepository.DELETE_LABEL) {
+                    insertRowNumber = i + 1;
+                    break;
+                }
+            }
+            const insertData = this.toStringList(entity);
+            if (insertRowNumber === -1) {
+                // 最後尾に挿入
+                this.sheet.appendRow(insertData);
+                return values.length + 1;
+            }
+            else {
+                // 削除行に挿入
+                this.getRowRange(insertRowNumber).setValues([insertData]);
+                return insertRowNumber;
+            }
+        });
+    }
+    /**
+     * 全件取得(フィルターなどはJSで実施)
+     */
+    getAll() {
+        let values;
+        values = this.onLock(() => {
+            const lastRow = this.sheet.getLastRow();
+            if (lastRow <= 1) {
+                // 0件の場合は取得しない
+                return [];
+            }
+            return this.sheet.getRange(2, 1, this.sheet.getLastRow() - 1, this.columnOrder.length + 1).getValues();
+        });
+        const entities = [];
+        for (const value of values) {
+            if (!value[0])
+                break;
+            if (value[0] === SSRepository.DELETE_LABEL)
+                continue;
+            entities.push(this.toEntity(value));
+        }
+        return entities;
+    }
+    /**
+     * １件取得
+     * @param row 取得するrow(rowは自動で付与され、不定一意)
+     */
+    getByRow(row) {
+        let stringList = [];
+        this.onLock(() => {
+            stringList = this.getRowRange(row).getValues()[0] ?? [];
+        });
+        return this.toEntity(stringList);
+    }
+    /**
+     * 更新処理(上書きなため、部分変更不可)
+     * @param entity 変更するデータ(row 必須)
+     */
+    update(entity) {
+        this.onLock(() => {
+            this.getRowRange(entity.row).setValues([this.toStringList(entity)]);
+        });
+    }
+    /**
+     * 削除処理
+     * @param row 削除するrow(rowは自動で付与され、不定一意)
+     */
+    delete(row) {
+        this.onLock(() => {
+            const range = this.getRowRange(row);
+            range.clear();
+            const d = new Array(this.columnOrder.length + 1);
+            d[0] = SSRepository.DELETE_LABEL;
+            range.setValues([d]);
+        });
+    }
+}
+/**
+ * SpreadsheetをDBとして利用する<br>
+ * 作成したRepositoryを登録する
+ */
+export function useSpreadsheetDB(initGlobal, ...repositoryList) {
+    const initTables = () => {
+        for (const repository of repositoryList) {
+            try {
+                console.info('create instances');
+                const r = new repository();
+                const name = r['tableName'];
+                console.info('start', name);
+                r.initTable();
+                console.info('success', name);
+            }
+            catch (e) {
+                console.error('init spreadsheet error', e);
+            }
+        }
+    };
+    initGlobal(global, initTables);
+}
+/*--------------------------------------------------------------------------------------------------------------------*/
+export function spreadsheetCache(spreadsheetId, expirationInSeconds) {
+    if (!useGasAPI.spreadsheetApp)
+        throw 'not found SpreadsheetApp. run "createGasApp({useGasAPI})"';
+    const spreadsheet = useGasAPI.spreadsheetApp.openById(spreadsheetId);
+    const tempSheet = spreadsheet.getSheetByName('cache');
+    const sheet = tempSheet ? tempSheet : spreadsheet.insertSheet().setName('cache');
+    return {
+        get: (rowNumber) => {
+            const expiration = Number.parseInt(sheet.getRange(rowNumber, 1, 1, 1).getValue(), 10);
+            if (!expiration) {
+                return null;
+            }
+            if (expirationInSeconds && (Date.now() - expiration) / 1000 > expirationInSeconds) {
+                return null;
+            }
+            const table = sheet.getRange(rowNumber, 2, 1, sheet.getLastColumn()).getValues();
+            let text = '';
+            for (const row of table) {
+                for (const col of row) {
+                    if (col) {
+                        text += col.toString();
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+            return JSON.parse(text);
+        },
+        set: (rowNumber, data) => {
+            // ※1セル50000文字制限のため、余裕を持って45000
+            let json = JSON.stringify(data);
+            const chunks = [Date.now()];
+            while (json.length > 0) {
+                chunks.push(json.substring(0, 45000));
+                json = json.substring(45000);
+            }
+            const range = sheet.getRange(rowNumber, 1, 1, chunks.length);
+            range.setValues([chunks]);
+        },
+        clear: (rowNumber) => {
+            sheet.getRange(rowNumber, 1, 1, sheet.getLastColumn()).clear();
         }
     };
 }

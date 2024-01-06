9f04a8713389d65eee2cc381a079818974ab0136
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-06 23:04:01 +0900

    test bin

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -1,28 +1,31 @@
+import fs from 'node:fs'
 
-import * as path from 'path'
-import * as fs from 'fs-extra'
-import {fileURLToPath} from 'node:url'
+fs.writeFileSync('test.txt', 'hello world')
 
-const cwd = process.cwd()
-const argTargetDir = formatTargetDir(process.argv[0])
-const defaultTargetDir = 'ham-vue3-gas-project'
-const targetDir = argTargetDir || defaultTargetDir
-const root = path.join(cwd, targetDir)
-console.log('root', root)
-const templateDir = path.resolve(
-  fileURLToPath(import.meta.url),
-  '../..',
-  `template`,
-)
-console.log('templateDir', templateDir)
-
-fs.copySync(path.join(templateDir, '.clasp.json'), path.join(root, '.clasp.json'))
-fs.copySync(path.join(templateDir, 'package.json'), path.join(root, 'package.json'))
-fs.copySync(path.join(templateDir, 'shared'), path.join(root, 'shared'))
-fs.copySync(path.join(templateDir, 'frontend'), path.join(root, 'frontend'))
-fs.copySync(path.join(templateDir, 'backend'), path.join(root, 'backend'))
-
-
-function formatTargetDir(targetDir) {
-      return targetDir?.trim().replace(/\/+$/g, '')
-}
+// import * as path from 'path'
+// import * as fs from 'fs-extra'
+// import {fileURLToPath} from 'node:url'
+//
+// const cwd = process.cwd()
+// const argTargetDir = formatTargetDir(process.argv[0])
+// const defaultTargetDir = 'ham-vue3-gas-project'
+// const targetDir = argTargetDir || defaultTargetDir
+// const root = path.join(cwd, targetDir)
+// console.log('root', root)
+// const templateDir = path.resolve(
+//   fileURLToPath(import.meta.url),
+//   '../..',
+//   `template`,
+// )
+// console.log('templateDir', templateDir)
+//
+// fs.copySync(path.join(templateDir, '.clasp.json'), path.join(root, '.clasp.json'))
+// fs.copySync(path.join(templateDir, 'package.json'), path.join(root, 'package.json'))
+// fs.copySync(path.join(templateDir, 'shared'), path.join(root, 'shared'))
+// fs.copySync(path.join(templateDir, 'frontend'), path.join(root, 'frontend'))
+// fs.copySync(path.join(templateDir, 'backend'), path.join(root, 'backend'))
+//
+//
+// function formatTargetDir(targetDir) {
+//       return targetDir?.trim().replace(/\/+$/g, '')
+// }

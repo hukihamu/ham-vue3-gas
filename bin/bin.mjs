6364d9cf028778c0427c0708bc43e995a9a22590
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-07 00:17:59 +0900

    Updated build scripts and added .gitignore
    Replaced chokidar-cli with nodemon in package.json file's build scripts for better compatibility. Also added .gitignore file that ignores node modules and other unnecessary files from version control.

--- bin/bin.mjs
+++ bin/bin.mjs
@@ -6,20 +6,18 @@ import fs from 'fs-extra'
 import {fileURLToPath} from 'url'
 import {execSync} from 'child_process'
 
-program.name('create-ham-vue3-gas').action(
-  () => {
-    console.log('Create ham-vue3-gas')
-    const rootPath = path.resolve(process.cwd())
-    const __dirname = path.dirname(fileURLToPath(import.meta.url))
-    fs.copySync(path.join(__dirname, 'template/.clasp.json'), path.join(rootPath, '.clasp.json'))
-    fs.copySync(path.join(__dirname, 'template/package.json'), path.join(rootPath, 'package.json'))
-    fs.copySync(path.join(__dirname, 'template/shared'), path.join(rootPath, 'shared'))
-    fs.copySync(path.join(__dirname, 'template/frontend'), path.join(rootPath, 'frontend'))
-    fs.copySync(path.join(__dirname, 'template/backend'), path.join(rootPath, 'backend'))
-    console.log('Successfully created')
-    console.log('npm install')
-    execSync('npm install')
-
-  }
-)
+program.name('create-ham-vue3-gas').action(() => {
+  console.log('Create ham-vue3-gas')
+  const rootPath = path.resolve(process.cwd())
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
+  fs.copySync(path.join(__dirname, 'template/.clasp.json'), path.join(rootPath, '.clasp.json'))
+  fs.copySync(path.join(__dirname, 'template/package.json'), path.join(rootPath, 'package.json'))
+  fs.copySync(path.join(__dirname, 'template/package.json'), path.join(rootPath, 'package.json'))
+  fs.copySync(path.join(__dirname, 'template/shared'), path.join(rootPath, 'shared'))
+  fs.copySync(path.join(__dirname, 'template/frontend'), path.join(rootPath, 'frontend'))
+  fs.copySync(path.join(__dirname, 'template/backend'), path.join(rootPath, 'backend'))
+  console.log('Successfully created')
+  console.log('npm install')
+  execSync('npm install')
+})
 program.parse(process.argv)
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Ham-Vue3-Gas
+
+## install
+```powershell
+npx git+https://github.com/hukihamu/ham-vue3-gas.git
+```

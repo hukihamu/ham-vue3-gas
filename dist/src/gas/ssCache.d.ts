6bd42fcebb7bde1acc0e466cb1d6ca9315c0af64
hukihamu <8084194+hukihamu @users.noreply.github.com>
2024-01-06 23:15:26 +0900

    test bin

--- dist/src/gas/ssCache.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="google-apps-script" />
-export declare function ssCache(spreadSheetApp: GoogleAppsScript.Spreadsheet.SpreadsheetApp, spreadsheetId: string, expirationInSeconds?: number): {
-    get: (rowNumber: number) => any;
-    set: (rowNumber: number, data: any) => void;
-    clear: (rowNumber: number) => void;
-};
